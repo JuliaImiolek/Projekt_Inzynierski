5997ff12247eba7b8597cf3a690d19f7c99fb9db
JuliaImiolek <94054461+JuliaImiolek@users.noreply.github.com>
2022-12-17 15:44:51 +0100

    Add reaction time test

---
+++ Projekt_Inzynierski/Controllers/CoordinationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt_Inzynierski.Controllers
+{
+    public class CoordinationController : Controller
+    {
+        public IActionResult ReactionTimeTest()
+        {
+            return View();
+        }
+    }
+}
---
+++ Projekt_Inzynierski/wwwroot/js/reactionTimeTestScript.js
@@ -0,0 +1,93 @@
+﻿var createdTime;
+var clickedTime;
+var reactionTime;
+var testName = "Big green box";
+var list = [];
+
+function makebox() {
+
+    var time = Math.random();
+    time = time * 10000; //speed nowy folder
+
+    setTimeout(function () {
+        document.getElementById('box').style.borderRadius = "0x";
+        document.getElementById('box').style.border = "5px solid black";
+
+
+
+        if (document.documentElement.clientWidth > document.documentElement.clientHeight) {
+            var dynamicBound = document.documentElement.clientWidth / 4;
+
+        } else {
+            var dynamicBound = document.documentElement.clientHeight / 10;
+        }
+
+        dynamicBound = dynamicBound + "px";
+        document.documentElement.style.setProperty("--bound", dynamicBound);
+
+
+
+        document.getElementById('box').style.backgroundColor = 'rgb(0, 255, 13)';
+        document.getElementById('box').style.display = "block";
+
+
+        createdTime = Date.now();
+    }, time);
+
+
+}
+
+function gameStart() {
+    var a = 0;
+    document.getElementById("userAge").style.display = 'none';
+    document.getElementById("userDevice").style.display = 'none';
+    document.getElementById("text").style.display = 'none';
+    document.getElementById('box').onclick = function () {
+        this.style.display = "none";
+        clickedTime = Date.now();
+        reactionTime = (clickedTime - createdTime) / 1000;
+        document.getElementById('time').innerHTML = reactionTime;
+        list[a] = reactionTime;
+        a++;
+        makebox();
+    }
+    makebox();
+}
+
+document.getElementById("SaveGame").onclick = function () {
+
+    var min = 10000;
+    for (var i = 0; i < list.length; i++) {
+
+        if (min >= list[i]) {
+            min = list[i];
+        }
+    }
+    var userAge = document.getElementById("userAge").value;
+    var userDevice = document.getElementById("userDevice").value;
+    console.log(userAge);
+    reactionSpan = min * 1000;
+    $.ajax({
+        url: '@Url.Action("AddRecordToReactionTest", "ReactionTest")',
+        type: 'POST',
+        data: {
+            "span": reactionSpan,
+            "testName": testName,
+            "userAge": userAge,
+            "userDevice": userDevice
+        },
+        success: function (response) {
+            var Data = JSON.parse(response);
+            alert("Your best reaction time has been saved!");
+        },
+        failure: function () {
+            var Data = JSON.parse(response);
+            alert(response);
+        },
+        error: function () {
+            var Data = JSON.parse(response);
+            alert(response);
+        }
+
+    });
+};
